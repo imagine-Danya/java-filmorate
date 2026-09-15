6faaad7a7e1f8eb932421c83b3bfeee3509d8d4e
Dany <ZakharovaDarya2021@yandex.ru>
2026-09-15 13:25:24 +0300

    Исправление стиля

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
 import lombok.Data;
+
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,12 +1,16 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.util.Collection;
 import java.util.Optional;
 
 public interface FilmStorage {
     Film create(Film film);
+
     Film update(Film film);
+
     Optional<Film> findById(Long id);
+
     Collection<Film> findAll();
 }
